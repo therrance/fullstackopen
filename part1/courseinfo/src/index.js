61e1144d41fe44a9edd3d930a9f528f920f90d0b
tgordienko <tgordienko@larch-networks.com>
2019-09-17 11:11:55 +0300

    Refactor to arrays

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -8,9 +8,9 @@ const Header = props => {
 const Content = props => {
   return (
     <div>
-      <Part part={props.part[0]} exercise={props.exercise[0]} />
-      <Part part={props.part[1]} exercise={props.exercise[1]} />
-      <Part part={props.part[2]} exercise={props.exercise[2]} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   );
 };
@@ -18,32 +18,44 @@ const Content = props => {
 const Part = props => {
   return (
     <p>
-      {props.part} {props.exercise}
+      {props.part.name} {props.part.exercise}
     </p>
   );
 };
 
 const Total = props => {
-  return <p>Number of exercises {props.exercises}</p>;
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
+    </p>
+  );
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7
+    },
+    {
+      name: "State of a component",
+      exercises: 14
+    }
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part={[part1, part2, part3]}
-        exercise={[exercises1, exercises2, exercises3]}
-      />
-      <Total exercises={exercises1 + exercises2 + exercises3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
